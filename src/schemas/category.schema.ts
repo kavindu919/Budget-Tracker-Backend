cc823bcd3ade86c945a9c51c6de7e1d4e8afccbc
kavindu919 <jayakodyk2@gmail.com>
2026-03-09 13:45:20 +0530

    fix: create missing schema for update

--- src/schemas/category.schema.ts
+++ src/schemas/category.schema.ts
@@ -1,6 +1,11 @@
 import z from "zod";
 export const categorySchema = z.object({
-  name: z.string().min(1).max(50),
+  id: z.string().optional(),
+  name: z.string({ message: "Category name is required" }).min(1).max(50),
   type: z.enum(["income", "expense"]),
   color: z.string().optional(),
+});
+
+export const updatecategorySchema = categorySchema.extend({
+  id: z.string().uuid("Invalid category ID"),
 });
